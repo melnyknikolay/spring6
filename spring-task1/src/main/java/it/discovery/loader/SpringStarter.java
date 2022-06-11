4e283b9e80c0b51d1956d39f8048afa0520aa4af
Sergiy <sergey.morenets@gmail.com>
2022-06-11 11:59:24 +0300

    Task 2

--- spring-task1/src/main/java/it/discovery/loader/SpringStarter.java
+++ spring-task1/src/main/java/it/discovery/loader/SpringStarter.java
@@ -1,7 +1,7 @@
 package it.discovery.loader;
 
 import it.discovery.model.Book;
-import it.discovery.repository.BookRepository;
+import it.discovery.service.BookService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Arrays;
@@ -11,15 +11,15 @@ public class SpringStarter {
         try (var context =
                      new AnnotationConfigApplicationContext("it.discovery")) {
 
-            BookRepository repository = context.getBean(BookRepository.class);
+            BookService service = context.getBean(BookService.class);
 
             Book book = new Book();
             book.setName("Introduction into Spring");
             book.setPages(100);
             book.setYear(2016);
-            repository.saveBook(book);
+            service.saveBook(book);
 
-            var books = repository.findBooks();
+            var books = service.findBooks();
             System.out.println(books);
             System.out.println("Total bean count = " + context.getBeanDefinitionCount());
             System.out.println("Bean identifiers: " + Arrays.toString(context.getBeanDefinitionNames()));
--- spring-task1/src/main/java/it/discovery/service/MainBookService.java
+++ spring-task1/src/main/java/it/discovery/service/MainBookService.java
@@ -2,9 +2,9 @@ package it.discovery.service;
 
 import it.discovery.model.Book;
 import it.discovery.repository.BookRepository;
-import it.discovery.repository.DBBookRepository;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
@@ -12,19 +12,20 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Getter
 @Setter
-public class MainBookService {
-    private final BookRepository repository =
-            new DBBookRepository();
+@Service
+public class MainBookService implements BookService {
+	private final BookRepository repository;
 
 	private boolean cachingEnabled;
 
 	private final Map<Integer, Book> bookCache = new ConcurrentHashMap<>();
 
-
-	public MainBookService() {
-		System.out.println("Using db repository");
+	public MainBookService(BookRepository repository) {
+		this.repository = repository;
+		System.out.println("Using repository " + repository.getClass());
 	}
 
+	@Override
 	public void saveBook(Book book) {
 		repository.saveBook(book);
 
@@ -33,6 +34,7 @@ public class MainBookService {
 		}
 	}
 
+	@Override
 	public Book findBookById(int id) {
 		if (cachingEnabled && bookCache.containsKey(id)) {
 			return bookCache.get(id);
@@ -42,6 +44,7 @@ public class MainBookService {
 		return repository.findBookById(id);
 	}
 
+	@Override
 	public List<Book> findBooks() {
 		return repository.findBooks();
 	}
